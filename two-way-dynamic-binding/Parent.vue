995cd918822f4836a07a0ca716d6487b794ad7c5
Hubert Sikorski <sikorski.sr@gmail.com>
2020-12-13 16:46:54 +0100

    init - add two way dynamic binding

---
+++ two-way-dynamic-binding/Parent.vue
@@ -0,0 +1,42 @@
+<template>
+  <child
+    v-model="value"
+    :alreadyChecked="value"
+    @change="changeValue"
+    :text="'I am the child'"
+  />
+</template>
+
+<script>
+import Child from './components/Child.vue';
+
+export default {
+  name: 'Parent',
+
+  components: {
+    Child,
+  },
+
+  data() {
+    return {
+      value: false,
+    }
+  },
+
+  mounted() {
+    this.changeValueOnMount();
+  },
+
+  methods: {
+    changeValueOnMount() {
+      setTimeout(() => {
+        this.value = true;
+      });
+    },
+
+    changeValue() {
+      this.value = !this.value;
+    },
+  },
+}
+</script>
---
+++ two-way-dynamic-binding/components/Child.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="g-checkbox-block">
+    <input
+      type="checkbox"
+      @change="$emit('change', checked)"
+      v-model="checked"
+      :disabled="disabled"
+    />
+    <p v-if="!(!!link)">{{ text }}</p>
+    <p v-if="!!link">
+      <a target="_blank" rel="noopener noreferrer" :href="link">{{ text }}</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'blueCheckbox',
+
+  model: {
+    prop: 'checked',
+    event: 'change',
+  },
+
+  props: {
+    text: String,
+    link: String,
+    disabled: Boolean,
+    alreadyChecked: Boolean,
+  },
+
+  data() {
+    return {
+      checked: false,
+    }
+  },
+
+  watch: {
+    alreadyChecked() {
+      this.checked = this.alreadyChecked;
+    },
+  },
+}
+</script>
